47772369ec1ddf4111103f0cb27c90d68a7b4153
Andrey Lebedev <lebed3ff.4n@yandex.ru>
2022-04-30 00:53:11 +0200

    secret jwt

--- utils/secretJwt.js
+++ utils/secretJwt.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 const { NODE_ENV, JWT_SECRET } = process.env;
 
 const secretJwt = {
-  JWT_SECRET: NODE_ENV === 'production' ? JWT_SECRET : 'super-strong-secret',
+  JWT_SECRET: NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret',
 };
 
 module.exports = secretJwt;
